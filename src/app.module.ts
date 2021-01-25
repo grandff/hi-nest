4327a7514c62440f88e17e32e4e03bdbe1c0bdb7
Yura Ko <dsaqaz421@naver.com>
2021-01-25 11:48:26 +0900

    2.0 Movies Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
 
 // decorater
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [MoviesController],
   providers: [],
 })
 export class AppModule {}
