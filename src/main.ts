29199ad90e164388ba4c3b9dd772f5ef7ea3195b
Yura Ko <dsaqaz421@naver.com>
2021-02-08 12:15:16 +0900

    2.4 DTOs and Validation part One

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist : true,
+      forbidNonWhitelisted : true,
+      transform : true,         // url에 찍히는 string value를 dto value 타입과 똑같도록 변환
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
